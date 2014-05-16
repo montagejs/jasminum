af0633a93840c2366eeaec8705968993b1c842b5
Kris Kowal <kris.kowal@cixar.com>
2014-05-16 12:54:18 -0700

    Phantom runner should load Jasmine from project
    Because npm does not necessarily install its dependencies if it is a
    dependency of the project under test.

--- phantom/runner.js
+++ phantom/runner.js
@@ -17,17 +17,22 @@ var argv = optimist
 search(argv._).then(function (files) {
     return Require.findPackageLocationAndModuleId(files[0])
     .then(function (found) {
+        return Require.loadPackage(found.location);
+    })
+    .then(function (found) {
+        return found.loadPackage({name: "jasminum"});
+    })
+    .then(function (found) {
         var path = Require.locationToPath(found.location);
         var modules = files.map(function (file) {
             return Fs.relativeFromDirectory(path, file);
         });
+        var jasminumLocation = found.getPackage({name: "jasminum"}).location;
+        var jasminumPath = Require.locationToPath(jasminumLocation);
+        var jasminumRelPath = Fs.relativeFromDirectory(path, jasminumPath);
+        var indexRelPath = Fs.join(jasminumRelPath, "phantom/index.html");
 
         var server = Joey
-        .route(function ($) {
-            $("~/...").fileTree(Fs.join(__dirname, ".."), {
-                followInsecureSymbolicLinks: true
-            });
-        })
         .fileTree(path, {followInsecureSymbolicLinks: true})
         .server();
 
@@ -37,7 +42,7 @@ search(argv._).then(function (files) {
             var port = server.address().port;
             var child = ChildProcess.spawn("phantomjs", [
                 Fs.join(__dirname, "script.js"),
-                "http://localhost:" + port + "/~/phantom/index.html?" + QS.stringify({
+                "http://localhost:" + port + "/" + indexRelPath + "?" + QS.stringify({
                     modules: modules,
                     failures: argv.f || argv.failures ? "show" : "hide",
                     passes: argv.p || argv.passes ? "show" : "hide",
@@ -49,6 +54,17 @@ search(argv._).then(function (files) {
                     process.stdout,
                     process.stderr
                 ]
+            });
+            child.on("error", function (cause) {
+                var error;
+                if (cause.code === "ENOENT") {
+                    error = new Error("Can't run tests. Install PhantomJS");
+                    error.code = cause.code;
+                    throw error;
+                } else {
+                    error = cause;
+                }
+                codeDeferred.reject(error);
             });
             child.on("close", function (code, signal) {
                 codeDeferred.resolve(code);
